d3f44207ac7ec1045ed19b090b95aae319077740
spinakker <friedl.mate@gmail.com>
2020-01-15 11:42:03 +0100

    new module and imports fixed

--- src/feladat1/feladat1.py
+++ src/feladat1/feladat1.py
@@ -1,6 +1,6 @@
 
 import femm
-import matplotlib.pyplot as plt
+from myfemm.geo import Rectangle
 
 class Params:
     Wd = 1.01
@@ -11,43 +11,6 @@ class Params:
     DTL = 0.15
     WT = 0.24
     EpsilonR = 4.4
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-class Rectangle:
-    def __init__(self, origin_x, origin_y, width, height):
-        self.origin_x = origin_x
-        self.origin_y = origin_y
-        self.width = width
-        self.height = height
-
-        self.draw()
-
-    def draw(self):
-        femm.ei_drawrectangle(self.origin_x, self.origin_y, self.origin_x + self.width, self.origin_y + self.height)
-
-    def get_midpoint(self):
-        midpoint =  Point(self.origin_x + self.width/2, self.origin_y + self.height/2)
-        return midpoint
-
-    def assign_conductor(self, conductor):
-        midpoint = self.get_midpoint()
-        femm.ei_selectsegment(self.origin_x, midpoint.y)
-        femm.ei_selectsegment(self.origin_x + self.width, midpoint.y)
-        femm.ei_selectsegment(midpoint.x, self.origin_y)
-        femm.ei_selectsegment(midpoint.x, self.origin_y + self.height)
-        femm.ei_setsegmentprop('<None>', 0, 1, 0, 0, conductor)
-        femm.ei_clearselected()
-
-    def assign_material(self, material):
-        midpoint = self.get_midpoint()
-        femm.ei_addblocklabel(midpoint.x, midpoint.y)
-        femm.ei_selectlabel(midpoint.x, midpoint.y)
-        femm.ei_setblockprop(material, 1, 0, 0)
-        femm.ei_clearselected()
 
 
 # The package must be initialized with the openfemm command.
