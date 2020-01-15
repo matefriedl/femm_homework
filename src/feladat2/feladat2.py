020697d0e4dff3a9815f7048630b7caf34528ecd
spinakker <friedl.mate@gmail.com>
2020-01-15 19:44:46 +0100

    feladat2 done

--- src/feladat2/feladat2.py
+++ src/feladat2/feladat2.py
@@ -16,9 +16,13 @@ class Params:
     Depth = 4
     Turns = 210
     core_gap = [0.1, 0.5, 1, 2]
+    coil_gap = 0.05
+    air_margin = 1
     ExcitationCurrent = 3
     CoreMaterial = "M-15 Steel"
+    CoilMaterial = "Copper"
 
+Params.core_gap.sort()
 
 femm.openfemm()
 femm.newdocument(ProblemType.MagnetoStatic.value)
@@ -26,6 +30,25 @@ Rectangle.problemType = ProblemType.MagnetoStatic
 femm.mi_probdef(0, 'centimeters', 'planar', 1.e-8, Params.Depth, 30)
 
 femm.mi_getmaterial(Params.CoreMaterial)
+femm.mi_getmaterial(Params.CoilMaterial)
+femm.mi_getmaterial('Air')
+
+femm.mi_addcircprop('Circuit', Params.ExcitationCurrent, 1) # 0 = paralel, 1 = series
+
+# name, A0, A1, A2, Phi, Mu, Sig, c0, c1, BdryFormat, ia, oa
+femm.mi_addboundprop('Boundary', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+# define air around everything
+
+air_width = 2 * Params.D4 + Params.D1 + Params.coil_gap * 2 + 2 * Params.air_margin
+air_height = 2 * Params.air_margin + Params.D7 + Params.core_gap[-1] + Params.D2
+air_rectangle = Rectangle(0 - Params.D4 - Params.coil_gap - Params.air_margin, 
+                          0 - Params.core_gap[-1] - Params.D7 - Params.air_margin,
+                          air_width,
+                          air_height)
+air_rectangle.assign_material('Air', 0, 0, Point(Params.D1/2, 0 - Params.core_gap[-1] - Params.D7 - Params.air_margin/2))
+air_rectangle.assign_boundary('Boundary')
+
 
 # define metal core 
 core_parts = []
@@ -45,15 +68,40 @@ core_parts.append(top_part)
 bottom_part = Rectangle(0, 0 - Params.core_gap[0] - Params.D7, Params.D1, Params.D7)
 core_parts.append(bottom_part)
 
-
-
 for core in core_parts:
     core.assign_material(Params.CoreMaterial, 0, 0)
+
+# define coil parts
+coil_origin_y = Params.D2 - Params.D8 - Params.D3
+coil_origins = [
+    Point(0 - Params.D4 - Params.coil_gap, coil_origin_y),
+    Point(Params.D5 + Params.coil_gap, coil_origin_y),
+    Point(Params.D1 - Params.coil_gap - Params.D4 - Params.D5, coil_origin_y),
+    Point(Params.D1 + Params.coil_gap, coil_origin_y),
+    ]
+for coil_origin in coil_origins:
+    coil = Rectangle(coil_origin.x, coil_origin.y, Params.D4, Params.D3)
+    coil.assign_material(Params.CoilMaterial, Params.Turns, 'Circuit')
 
 # Now, the finished input geometry can be displayed.
 femm.mi_zoomnatural()
 
 # We have to give the geometry a name before we can analyze it.
 femm.mi_saveas('feladat2.fem')
+
+move_vectors = []
+for previous, current in zip(Params.core_gap, Params.core_gap[1:]):
+    move_vectors.append(Point(0, previous - current))
+
+forces = {}
+for core_gap, move_vector in zip(Params.core_gap, move_vectors):
+    femm.mi_analyze()
+    femm.mi_loadsolution()
+    bottom_part.select_block()
+    forces[core_gap] = femm.mo_blockintegral(19)
+    bottom_part.clearselected()
+    femm.mi_selectgroup(bottom_part.index)
+    bottom_part.move(move_vector.x, move_vector.y)
+    print(f'In case of {core_gap}, force is {forces[core_gap]}')
 
 femm.closefemm()
--- src/myfemm/geo.py
+++ src/myfemm/geo.py
@@ -19,9 +19,13 @@ Prefixes = {
 class Rectangle:
     problemType: ProblemType = None
     prefix: str = None
+    next_index: int = 0
 
     def __init__(self, origin_x, origin_y, width, height):
         assert isinstance(self.problemType, ProblemType), f"No problem type specified"
+
+        self.index = Rectangle.next_index
+        Rectangle.next_index += 1
         self.origin_x = origin_x
         self.origin_y = origin_y
         self.width = width
@@ -30,6 +34,14 @@ class Rectangle:
         self.prefix = Prefixes[self.problemType]
 
         self.draw()
+        self.select_periphery()
+        if self.problemType is ProblemType.ElectroStatic:
+            femm.ei_setsegmentprop('<None>', 0, 1, 0, self.index, '<None>')
+        elif self.problemType is ProblemType.MagnetoStatic:
+            femm.mi_setsegmentprop('<None>', 0, 1, 0, self.index)
+        else:
+            assert False
+        self.clearselected()
 
     def callCommandWithPrefix(self, command):
         return femm.callfemm(f'{self.prefix}{command}')
@@ -57,6 +69,13 @@ class Rectangle:
         self.addnode(end_x, end_y)
         self.addsegment(start_x, start_y, end_x, end_y)
 
+    def move(self, dx, dy):
+        self.select_periphery()
+        self.callCommandWithPrefix(f'movetranslate({dx},{dy})')
+        self.origin_x += dx
+        self.origin_y += dy
+        self.clearselected()
+
     def draw(self):
         x2 = self.origin_x + self.width
         y2 = self.origin_y + self.height
@@ -71,26 +90,48 @@ class Rectangle:
 
     def assign_conductor(self, conductor):
         assert self.problemType is ProblemType.ElectroStatic
+        self.select_periphery()
+        femm.ei_setsegmentprop('<None>', 0, 1, 0, 0, conductor)
+        self.clearselected()
 
+    # todo can be unified with function above
+    def assign_boundary(self, boundary):
+        assert self.problemType is ProblemType.MagnetoStatic
+        self.clearselected()
+        self.select_periphery()
+        femm.mi_setsegmentprop('Boundary', 0, 1, 0, 0)
+        self.clearselected()
+
+    # always clearselected after calling
+    def select_periphery(self):
+        self.clearselected()
         midpoint = self.get_midpoint()
         self.selectsegment(self.origin_x, midpoint.y)
         self.selectsegment(self.origin_x + self.width, midpoint.y)
         self.selectsegment(midpoint.x, self.origin_y)
         self.selectsegment(midpoint.x, self.origin_y + self.height)
-        femm.ei_setsegmentprop('<None>', 0, 1, 0, 0, conductor)
-        self.clearselected()
 
-    def assign_material(self, material, turns = None, circuit = None):
+    def select_block(self):
         midpoint = self.get_midpoint()
-        self.addblocklabel(midpoint.x, midpoint.y)
-        self.selectlabel(midpoint.x, midpoint.y)
         if self.problemType is ProblemType.ElectroStatic:
-            femm.ei_setblockprop(material, 1, 0, 0)
+            femm.eo_selectblock(midpoint.x, midpoint.y)
+        elif self.problemType is ProblemType.MagnetoStatic:
+            femm.mo_selectblock(midpoint.x, midpoint.y)
+        else:
+            assert False
+
+    def assign_material(self, material, turns = None, circuit = None, where: Point = None):
+        if where is None:
+            where = self.get_midpoint()
+        self.addblocklabel(where.x, where.y)
+        self.selectlabel(where.x, where.y)
+        if self.problemType is ProblemType.ElectroStatic:
+            femm.ei_setblockprop(material, 1, 0, self.index)
         elif self.problemType is ProblemType.MagnetoStatic:
             assert turns is not None
             assert circuit is not None
             # blockname, automesh, meshsize, incircuit, magdir, group, turns
-            femm.mi_setblockprop(material, 1, 0, circuit, 0, 0, turns)
+            femm.mi_setblockprop(material, 1, 0, circuit, 0, self.index, turns)
         self.clearselected()
 
 
