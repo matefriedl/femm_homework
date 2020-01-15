8fe8a9001b85090d079d4985bc8485b19a3aca14
spinakker <friedl.mate@gmail.com>
2020-01-15 13:43:59 +0100

    drawing refactor

--- src/myfemm/geo.py
+++ src/myfemm/geo.py
@@ -16,8 +16,6 @@ Prefixes = {
 }
 
 
-
-
 class Rectangle:
     problemType: ProblemType = None
     prefix: str = None
@@ -33,11 +31,30 @@ class Rectangle:
 
         self.draw()
 
+    def callCommandWithPrefix(self, command):
+        return femm.callfemm(f'{self.prefix}{command}')
+
     def selectsegment(self, x, y):
-        return femm.callfemm(f'{self.prefix}selectsegment(' + femm.numc(x) + femm.num(y) + ')')
+        return self.callCommandWithPrefix(f'selectsegment({x},{y})')
+
+    def addnode(self, x, y):
+        return self.callCommandWithPrefix(f'addnode({x},{y})')
+
+    def addsegment(self, start_x, start_y, end_x, end_y):
+        return self.callCommandWithPrefix(f'addsegment({start_x},{start_y},{end_x}, {end_y})')
+
+    def drawline(self, start_x, start_y, end_x, end_y):
+        self.addnode(start_x, start_y)
+        self.addnode(end_x, end_y)
+        self.addsegment(start_x, start_y, end_x, end_y)
 
     def draw(self):
-        femm.ei_drawrectangle(self.origin_x, self.origin_y, self.origin_x + self.width, self.origin_y + self.height)
+        x2 = self.origin_x + self.width
+        y2 = self.origin_y + self.height
+        self.drawline(self.origin_x, self.origin_y, x2, self.origin_y)
+        self.drawline(x2, self.origin_y, x2, y2)
+        self.drawline(x2, y2, self.origin_x, y2)
+        self.drawline(self.origin_x, y2, self.origin_x, self.origin_y)
 
     def get_midpoint(self):
         midpoint =  Point(self.origin_x + self.width/2, self.origin_y + self.height/2)
@@ -45,7 +62,7 @@ class Rectangle:
 
     def assign_conductor(self, conductor):
         assert self.problemType is ProblemType.ElectroStatic
-        
+
         midpoint = self.get_midpoint()
         self.selectsegment(self.origin_x, midpoint.y)
         self.selectsegment(self.origin_x + self.width, midpoint.y)
@@ -60,3 +77,21 @@ class Rectangle:
         femm.ei_selectlabel(midpoint.x, midpoint.y)
         femm.ei_setblockprop(material, 1, 0, 0)
         femm.ei_clearselected()
+
+
+if __name__ == "__main__":
+    femm.openfemm()
+
+    # We need to create a new Electrostatic (ei) document to work on.
+    femm.newdocument(ProblemType.ElectroStatic.value)
+
+    # Define the problem type. Units of mm; Axisymmetric; 
+    # Precision of 10^(-8) for the linear solver; a placeholder of 0 for 
+    # the depth dimension, and an angle constraint of 30 degrees
+    femm.ei_probdef('centimeters', 'planar', 1.e-8, 0, 30)
+
+    Rectangle.problemType = ProblemType.ElectroStatic
+    rectangle = Rectangle(0, 0, 10, 10)
+    rectangle.addnode(5, 5)
+    femm.ei_zoomnatural()
+    femm.closefemm()
